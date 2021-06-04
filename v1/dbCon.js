5bb186089cca5b4335c240fad9b38299232cc6aa
shera <ravishekhar5588@gmail.com>
2021-06-05 00:47:11 +0530

    remove env for testing

--- v1/dbCon.js
+++ v1/dbCon.js
@@ -14,13 +14,21 @@ env.config();
 
 
 
+// const pool = new Pool({
+//   user: process.env.dbUsername,
+//   host: process.env.dbHost,
+//   database: process.env.dbDatabase,
+//   password: process.env.dbPassword,
+//   port: process.env.dbPort
+// });
+
 const pool = new Pool({
-  user: process.env.dbUsername,
-  host: process.env.dbHost,
-  database: process.env.dbDatabase,
-  password: process.env.dbPassword,
-  port: process.env.dbPort
-});
+    user: 'cvhuamkgjdeqib',
+    host: 'ec2-3-212-75-25.compute-1.amazonaws.com',
+    database: 'dbjuru4artr58i',
+    password: 'eba5f9e3e5031bc397862b505fbeaf283bdde3dddace64cce3ca66b79c115e65',
+    port: '5432'
+  });
 
 module.exports = pool;
 
