95695ea5e34c34616c4da961b943bab90c3e256a
shera <ravishekhar5588@gmail.com>
2021-06-06 16:45:50 +0530

    manual password entry is removed from dbCon file

--- v1/dbCon.js
+++ v1/dbCon.js
@@ -11,14 +11,6 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
-// const pool = new Pool({
-//     user: 'cvhuamkgjdeqib',
-//     host: 'ec2-3-212-75-25.compute-1.amazonaws.com',
-//     database: 'dbjuru4artr58i',
-//     password: 'eba5f9e3e5031bc397862b505fbeaf283bdde3dddace64cce3ca66b79c115e65',
-//     port: '5432',
-//     ssl: { rejectUnauthorized: false }
-//   });
 
 module.exports = pool;
 
